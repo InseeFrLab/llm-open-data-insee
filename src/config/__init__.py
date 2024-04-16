d5d5ab6077a7565edee81a4592e292c2f34db40f
frole <francois.role@finances.gouv.fr>
2024-04-16 12:23:27 +0200

    Adding an s3-based loading function

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -2,3 +2,5 @@ from .loading_config import CHUNK_OVERLAP, CHUNK_SIZE
 from .embed_config import EMB_MODEL_NAME, EMB_DEVICE
 from .db_config import DB_DIR
 from .llm_config import MODEL_DEVICE, MODEL_NAME
+from .s3_config import S3_ENDPOINT_URL
+
--- src/utils/loading_utilities.py
+++ src/utils/loading_utilities.py
@@ -1,12 +1,15 @@
 import logging
 import os
 import pandas as pd
-#import s3fs
+import s3fs
+
+from config import S3_ENDPOINT_URL
 
 def load_dataframe_from_parquet(path: str, engine: str = 'fastparquet'):
     return pd.read_parquet(path, engine)
 
-#def load_dataframe_from_parquet_using_S3(S3_FILE_PATH, engine: str = 'fastparquet'):
-#    fs = s3fs.S3FileSystem(client_kwargs={'endpoint_url': S3_ENDPOINT_URL})
-#    with fs.open(S3_FILE_PATH, mode="rb") as file_in:
-#        return pd.read_parquet(file_in, engine)
+def load_dataframe_from_parquet_using_S3(filepath: str, engine: str = 'fastparquet'):
+    fs = s3fs.S3FileSystem(client_kwargs={'endpoint_url': S3_ENDPOINT_URL})
+    with fs.open(filepath, mode="rb") as file_in:
+        return pd.read_parquet(file_in, engine)
+
