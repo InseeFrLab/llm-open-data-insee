f4e85d0710142d5b9ca442cdf14206836a5db802
frole <francois.role@finances.gouv.fr>
2024-04-18 08:45:46 +0200

    Adding code in function build_llm_model to allow for the loading of larger models

--- src/model_building/build_llm_model.py
+++ src/model_building/build_llm_model.py
@@ -1,15 +1,32 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline,  BitsAndBytesConfig
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 
 from config import MODEL_DEVICE, MODEL_NAME
 
 
-def build_llm_model() -> HuggingFacePipeline:
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndBytesConfig
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+
+def build_llm_model(quantization=True) -> HuggingFacePipeline:
     """
     Create the llm model
     """
-    model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, load_in_8bit=True, device_map=MODEL_DEVICE)
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, device_map=MODEL_DEVICE)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, device_map='cuda')
+    if quantization:
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype="float16",
+            bnb_4bit_use_double_quant=False,
+        )
+        model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, 
+                                                      quantization_config=bnb_config,
+                                                      )
+        
+    else:
+        model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, device_map='cuda')
+       
     return HuggingFacePipeline(
         pipeline=pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=2000)
     )
+
