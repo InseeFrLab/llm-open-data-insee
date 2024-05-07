05c8702c948d8aaad3e7c9968d6481b4a2745e58
Thomas Faria <thomasfaria31@gmail.com>
2024-05-07 16:34:37 +0000

    prise en compte main

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -3,6 +3,7 @@ from .llm_config import MODEL_NAME, MODEL_DEVICE
 from .embed_config import EMB_MODEL_NAME, EMB_DEVICE
 from .data_paths_config import LOG_FILE_PATH
 from .s3_config import S3_ENDPOINT_URL
+from .prompting_config import BASIC_RAG_PROMPT_TEMPLATE, RAG_PROMPT_TEMPLATE
 
 __all__ = [
     "DB_DIR",
@@ -13,4 +14,6 @@ __all__ = [
     "EMB_DEVICE",
     "LOG_FILE_PATH",
     "S3_ENDPOINT_URL",
+    "BASIC_RAG_PROMPT_TEMPLATE",
+    "RAG_PROMPT_TEMPLATE",
 ]
