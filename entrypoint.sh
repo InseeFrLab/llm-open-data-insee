dd551c638170bf0952e6e7fad04070790868f345
avouacr <avouacr@pm.me>
2024-05-15 12:53:10 +0000

    prod mode deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 mc cp --recursive s3/$S3_BUCKET/data/chroma_database/chroma_db/ data/chroma_db
 
 # Run app
-chainlit run app.py --host 0.0.0.0 -w -h
+chainlit run app.py --host 0.0.0.0 --port 8000 -h
