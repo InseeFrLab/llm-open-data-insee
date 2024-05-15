4d3102a767061f97cd7f3315ecbdac065bf45c52
avouacr <avouacr@pm.me>
2024-05-15 13:25:54 +0000

    mc env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Import vector DB
+MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
 mc cp --recursive s3/$S3_BUCKET/data/chroma_database/chroma_db/ data/chroma_db
 
 # Run app
