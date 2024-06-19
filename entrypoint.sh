a8eccd821f901c3d409e2257b92b43010ff2b3dc
avouacr <avouacr@pm.me>
2024-06-19 14:42:17 +0000

    variabilize stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,10 @@
 
 # Import vector DB
 export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
-mc cp --recursive s3/projet-llm-insee-open-data/data/chroma_database/chroma_db/ data/chroma_db
+mc cp --recursive s3/$S3_BUCKET/data/chroma_database/chroma_db/ data/chroma_db
+
+# Cache LLM
+python src/fetch_llm_model.py
 
 # Run app
 chainlit run app.py --host 0.0.0.0 --port 8000 -h
