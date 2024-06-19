e02f1b27d2676cf3c559c4711f8e863befd63dff
avouacr <avouacr@pm.me>
2024-06-19 15:45:31 +0000

    uncomment

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,9 @@
 
 # Import vector DB
 export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
-export S3_BUCKET='projet-llm-insee-open-data'
-# mc cp --recursive s3/$S3_BUCKET/data/chroma_database/chroma_db/ data/chroma_db
+mc cp --recursive s3/$S3_BUCKET/data/chroma_database/chroma_db/ data/chroma_db
 
 # Cache LLM
-export LLM_MODEL_NAME=mistralai/Mistral-7B-Instruct-v0.2
-export EMB_MODEL_NAME=OrdalieTech/Solon-embeddings-large-0.1
 python -m src.model_building.fetch_llm_model 2>&1
 
 # Run app
