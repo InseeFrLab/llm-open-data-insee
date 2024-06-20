a64270500e449c2e6ef09a1c975cc004be6417fe
avouacr <avouacr@pm.me>
2024-06-20 17:46:21 +0000

    fix var name

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDP
 mc cp --recursive s3/$S3_BUCKET/data/chroma_database/chroma_db/ data/chroma_db
 
 # Fetch cached LLM from S3 if available
-MODEL_NAME_HF=$(echo "$MODEL_NAME" | sed 's|/|--|g' | sed 's|^|models--|')
-MODEL_PATH_S3=s3/$S3_BUCKET/models/hf_hub/$MODEL_NAME_HF
+LLM_MODEL_NAME_HF=$(echo "$LLM_MODEL_NAME" | sed 's|/|--|g' | sed 's|^|models--|')
+MODEL_PATH_S3=s3/$S3_BUCKET/models/hf_hub/$LLM_MODEL_NAME_HF
 if mc ls $MODEL_PATH_S3 > /dev/null 2>&1; then
     echo "Fetching cached LLM $MODEL_NAME from S3."
-    mc cp --recursive $MODEL_PATH_S3/ $HOME/.cache/huggingface/hub/$MODEL_NAME_HF
+    mc cp --recursive $MODEL_PATH_S3/ $HOME/.cache/huggingface/hub/$LLM_MODEL_NAME_HF
 fi
 
 # Run app
