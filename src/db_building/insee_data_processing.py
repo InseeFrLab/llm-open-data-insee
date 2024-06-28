ed0158ca01357d9db57da0729cf24161a9e1269e
ThomasFaria <thomas.faria@ensae.fr>
2024-06-28 15:08:10 +0000

    initialize work on db building pipeline

--- src/db_building/insee_data_processing.py
+++ src/db_building/insee_data_processing.py
@@ -1,62 +1,34 @@
 import logging
-import sys
 
 import pandas as pd
 import s3fs
-
-sys.path.append("./src")
-
 from config import S3_BUCKET, S3_ENDPOINT_URL
 from utils_db import complete_url_builder
 
-if __name__ == "__main__":
+FILES = [
+    "applishare_extract",
+    "solr_extract",
+    # "rmes_extract_sources",
+    # "applishare_extract_indicateurs",
+]
+
+
+def main():
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-    # Create filesystem object
     fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": S3_ENDPOINT_URL})
-    BUCKET = S3_BUCKET + "/data/raw_data"
 
-    # S3_BUCKET = "projet-llm-insee-open-data/data/raw_data"
+    tables = {file: pd.read_parquet(f"s3://{S3_BUCKET}/data/raw_data/{file}.parquet", filesystem=fs) for file in FILES}
 
-    FILE_KEY_S3 = "applishare_extract.parquet"
-    FILE_PATH_S3 = BUCKET + "/" + FILE_KEY_S3
+    for key, table in tables.items():
+        logging.info(f"Size of {key} : {len(table)}")
 
-    logging.info(FILE_PATH_S3)
-    with fs.open(FILE_PATH_S3, mode="rb") as file_in:
-        table_app = pd.read_parquet(file_in, engine="fastparquet")
+    joined_table = tables["applishare_extract"].merge(tables["solr_extract"], how="inner", on="id")
+    joined_table["url"] = complete_url_builder(joined_table)
+    subset_table = joined_table.reset_index(drop=True)[["id", "titre", "categorie", "url", "dateDiffusion", "xml_content"]]
+    subset_table["dateDiffusion"] = pd.to_datetime(subset_table["dateDiffusion"], format="mixed")
+    subset_table.to_parquet(f"s3://{S3_BUCKET}/data/raw_data/applishare_solr_joined.parquet", filesystem=fs)
 
-    FILE_KEY_S3 = "solr_extract.parquet"
-    FILE_PATH_S3 = BUCKET + "/" + FILE_KEY_S3
-    logging.info(FILE_PATH_S3)
 
-    with fs.open(FILE_PATH_S3, mode="rb") as file_in:
-        table_solr = pd.read_parquet(file_in, engine="fastparquet")
-
-    FILE_KEY_S3 = "rmes_extract_sources.parquet"
-    FILE_PATH_S3 = BUCKET + "/" + FILE_KEY_S3
-    logging.info(FILE_PATH_S3)
-    with fs.open(FILE_PATH_S3, mode="rb") as file_in:
-        table_source = pd.read_parquet(file_in, engine="fastparquet")
-
-    FILE_KEY_S3 = "applishare_extract_indicateurs.parquet"
-    FILE_PATH_S3 = BUCKET + "/" + FILE_KEY_S3
-    logging.info(FILE_PATH_S3)
-    with fs.open(FILE_PATH_S3, mode="rb") as file_in:
-        table_indic = pd.read_parquet(file_in, engine="fastparquet")
-
-    logging.info("End of loading documents")
-    logging.info(f"size table_solr : {len(table_solr)}")
-    logging.info(f"size table_app : {len(table_app)}")
-    logging.info(f"size table_source : {len(table_source)}")
-    logging.info(f"size table_indic : {len(table_indic)}")
-
-    logging.info("Merging Table_solr and table_app operation on id")
-    joined_table = table_app.merge(table_solr, how="inner", on="id")
-
-    logging.info("Rebuilding URL")
-    urls_pd_serie = complete_url_builder(joined_table)
-    joined_table = joined_table.drop("url", axis=1)  # remove url column if exist
-    joined_table.insert(3, "url", urls_pd_serie)
-
-    logging.info("Storing tables")
-    joined_table.to_csv("data_complete.csv", index=False)
+if __name__ == "__main__":
+    main()
