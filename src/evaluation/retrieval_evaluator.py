8989d5c7a47b542ddbf7a94b1fcc135b6654e7d6
mbesnier <matthias.besnier@dauphine.eu>
2024-06-28 10:41:00 +0000

    implement reranking feature in Retrieval Evaluation package

--- src/evaluation/retrieval_evaluator.py
+++ src/evaluation/retrieval_evaluator.py
@@ -12,6 +12,7 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores.chroma import Chroma
 
 from evaluation import RetrievalConfiguration
+from retrieval_evaluation_measures import recall, precision, hit_rate, mrr
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
@@ -26,34 +27,6 @@ def build_embedding_model(model_name):
         show_progress=False
     )
 
-## Measures #############
-
-def recall(retrieved, relevant):
-    intersection = set(retrieved) & set(relevant)
-    return np.round(len(intersection) / len(relevant), 3)  if len(relevant) > 0 else 0
-
-def precision(retrieved, relevant):
-    intersection = set(retrieved) & set(relevant)
-    return np.round(len(intersection) / len(retrieved), 3)  if len(retrieved) > 0 else 0
-
-def hit_rate(retrieved, relevant):
-    """ 
-    Hit rate metric is equivalent to the accuracy 
-    """
-    correct_retrieved = sum(1 for pred, label in zip(retrieved, relevant) if pred == label)
-    total_retrieved = len(retrieved)
-    hit_rate = correct_retrieved / total_retrieved
-    return hit_rate
-
-def mmr(retrieved, relevant):
-    # compute Mean Reciprocal Rank (Order Aware Metrics)
-    if relevant not in retrieved:
-        mrr_score = 1/np.inf
-    else : 
-        rank_q = retrieved.index(relevant)
-        mrr_score = 1/(rank_q + 1)
-    return mrr_score
-
 ## Main class ###########
 
 class RetrievalEvaluator:
@@ -63,6 +36,10 @@ class RetrievalEvaluator:
             vector_db : Chroma,
             dataframe_dict: Dict[str, pd.DataFrame],
            ) -> Dict[str, Tuple[csr_matrix, Dict, Dict, Dict]]:
+
+        """
+        vector_db : Chroma Test database, make sure the questions are based on the dataframe documents
+        """
         results = {}
         for df_name, df in dataframe_dict.items():
             results[df_name] = {}
@@ -99,7 +76,7 @@ class RetrievalEvaluator:
                     all_individual_recalls.append(individual_recalls)
                     all_individual_precisions.append(individual_precisions)
                 # length of all_individual_recalls/precisions equals len(configuration.k_values)
-                assert len(df) == len(all_individual_recalls), "nb of indivduals error"
+                assert len(df) == len(all_individual_recalls), "nb of individuals error"
                 logging.info(f"      length of all_individual recalls is {len(all_individual_recalls)}")
                 all_k_precisions = list(zip(*all_individual_precisions))
                 all_k_recalls = list(zip(*all_individual_recalls))
--- src/evaluation/utils.py
+++ src/evaluation/utils.py
@@ -12,6 +12,26 @@ from langchain.docstore.document import Document as LangchainDocument
 from rank_bm25 import BM25Okapi, BM25L, BM25Plus
 from ragatouille import RAGPretrainedModel
 
+#langchain packages
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_core.runnables import RunnablePassthrough, RunnableParallel
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableLambda, RunnableParallel, RunnablePassthrough
+
+#loading rerankers
+from langchain.retrievers import EnsembleRetriever
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import CrossEncoderReranker
+from langchain_community.cross_encoders import HuggingFaceCrossEncoder
+from langchain_community.retrievers import BM25Retriever
+from typing import Any, List, Optional, Sequence, Dict
+from langchain.retrievers.document_compressors.base import BaseDocumentCompressor
+from ragatouille import RAGPretrainedModel 
+from langchain.schema import Document
+from pydantic import BaseModel, Field
+
+
 def use_sbert_retrieval_evaluator(df: pd.DataFrame, 
                                   model: SentenceTransformer) -> Dict:
     """
@@ -44,7 +64,7 @@ def use_sbert_retrieval_evaluator(df: pd.DataFrame,
     )
     return ir_evaluator(model)
 
-
+"""
 def rerank_with_ColBERT(reranker, query : str, retrieved_docs : List[LangchainDocument], filter_k : int ):
     relevant_docs = [doc.page_content for doc in retrieved_docs]  # keep only text
     reranked_docs = reranker.rerank(query=query, documents=relevant_docs, k=filter_k)
@@ -56,7 +76,7 @@ def rerank_with_BM25(model_class, query : str, retrieved_docs : List[LangchainDo
     relevant_docs = [doc.page_content for doc in retrieved_docs]
     bm25 = model_class(relevant_docs)
     tokenized_query = query.split()
-    return bm25.get_top_n(tokenized_query, retrieved_docs, n=filter_k)
+    return bm25.get_top_n(tokenized_query, retrieved_docs, n=filter_k)"""
 
 
 def rerank_with_metadata(reranker, query : str, retrieved_docs : List[LangchainDocument], filter_k : int , params : Dict):
@@ -85,3 +105,108 @@ def rerank_with_metadata(reranker, query : str, retrieved_docs : List[LangchainD
     reordered_docs = [source_to_doc_map[new_doc.metadata["source"]] for new_doc in new_retrieved_docs if new_doc.metadata["source"] in source_to_doc_map]
 
     return reordered_docs
+
+# Define the compression function
+def compress_BM25_lambda(documents: Sequence[Document], query: str, k: int = 5, **kwargs: Dict[str, Any]) -> Sequence[Document]:
+    """Compress retrieved documents given the query context."""
+
+    # Initialize the retriever with the documents
+    retriever = BM25Retriever.from_documents(documents, k=k, **kwargs)
+    return retriever.get_relevant_documents(query)
+
+# Define the compression function using Metadata
+def compress_Metadata_lambda(documents: Sequence[LangchainDocument], query: str, config: Dict) -> Sequence[LangchainDocument]:
+    
+    rerank_k = config.get("rerank_k", 5)
+    metadata_field = config.get("use_metadata", None)
+
+    if metadata_field is not None: 
+        new_data = []
+        for doc in documents:
+            meta = doc.metadata
+            if metadata_field in meta and len(meta[metadata_field]) > 0:
+                page_content = meta[metadata_field]
+            else:
+                page_content = doc.page_content
+            
+            new_data.append(
+                LangchainDocument(
+                    page_content=page_content, 
+                    metadata={"source": meta.get("source", "unknown")}
+                )
+            )
+
+        # Use BM25 to rerank the new data
+        new_retrieved_docs = compress_BM25_lambda(documents=new_data, query=query, k=rerank_k)
+
+        # Map sources to original documents
+        source_to_doc_map = {doc.metadata.get("source", "unknown"): doc for doc in documents}
+        
+        # Return the reranked documents based on the source mapping
+        return [source_to_doc_map[new_doc.metadata["source"]] for new_doc in new_retrieved_docs if new_doc.metadata["source"] in source_to_doc_map]
+    else:
+        # If no metadata field specified, return top k documents
+        return documents[:rerank_k]
+
+
+def choosing_reranker(base_retriever, reranker_type , reranker_name, rerank_k): 
+    """ 
+    Return a langchain compatible augmented retriever. 
+    """
+    if reranker_type is None:
+        retrieval_agent = base_retriever 
+
+    elif reranker_type == "BM25":
+        retrieval_agent = (
+            RunnableParallel({"documents": base_retriever, "query": RunnablePassthrough()})
+            | RunnableLambda(lambda r: compress_BM25_lambda(documents=r["documents"], query=r["query"], k=rerank_k))
+        )
+    elif reranker_type == "Cross-encoder":
+        model = HuggingFaceCrossEncoder(model_name=reranker_name)
+        compressor = CrossEncoderReranker(model=model, top_n=rerank_k)
+        retrieval_agent = ContextualCompressionRetriever(base_compressor=compressor, base_retriever=base_retriever)
+    
+    elif reranker_type == "ColBERT":
+        colBERT = RAGPretrainedModel.from_pretrained(reranker_name)
+        retrieval_agent = ContextualCompressionRetriever(
+            base_compressor=colBERT.as_langchain_document_compressor(k=rerank_k), base_retriever=base_retriever
+        )
+    return retrieval_agent
+
+
+def build_chain_retriever(base_retriever, config = Dict):
+    """ 
+    Build a langchain chain without generation, focusing on retrieving right documents 
+    Customizable Implementation for testing different retrieving strategy. 
+    """
+    # {"reranker_type" : ..., "reranker_name": ..., "rerank_k" : < "nb_retrieved", "param_ensemble" : [{"reranker_type" : ..., "reranker_name" : ..., "reranker_weight"} )]}
+
+    reranker_type = config.get("reranker_type", None)
+    reranker_name = config.get("reranker_name", None)
+    rerank_k = config.get("rerank_k", 5)
+    
+    if reranker_type in ["BM25","Cross-encoder","ColBERT"]:
+        retrieval_agent = choosing_reranker(base_retriever, reranker_type , reranker_name, rerank_k)
+
+    elif reranker_type == "Ensemble":
+
+        weigths = []
+        base_retrievers = []
+        for config in config.get("param_ensemble", []):
+            (r_type, r_name, r_w) = config.values()
+            reranker_model = choosing_reranker(base_retriever, r_type , r_name, rerank_k)
+
+            #add model to the lis of rerankers
+            base_retrievers.append(reranker_model)
+            weigths.append(r_w)
+        
+        #build ensemble model
+
+        weights = [1/len(weights) for _ in weights] if np.sum(weights) !=1 else weights
+        
+        retrieval_agent = EnsembleRetriever(
+            base_retrievers=base_retrievers, 
+            weigths=weights
+        )
+
+    return retrieval_agent
