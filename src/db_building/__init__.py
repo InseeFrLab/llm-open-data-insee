eba7659224b0841112d2de98004b1bdc85dabc87
ThomasFaria <thomas.faria@ensae.fr>
2024-07-05 15:35:29 +0000

    remove unecessary file

--- src/db_building/__init__.py
+++ src/db_building/__init__.py
@@ -1,11 +1,9 @@
 from .build_database import (
-    build_database_from_dataframe,
     build_vector_database,
     reload_database_from_local_dir,
 )
 
 __all__ = [
     "build_vector_database",
-    "build_database_from_dataframe",
     "reload_database_from_local_dir",
 ]
--- src/db_building/build_database.py
+++ src/db_building/build_database.py
@@ -3,8 +3,7 @@ import logging
 import pandas as pd
 import s3fs
 from chromadb.config import Settings
-from config import COLLECTION_NAME, DB_DIR_LOCAL, DB_DIR_S3, EMB_DEVICE, EMB_MODEL_NAME, MARKDOWN_SEPARATORS, S3_BUCKET
-from doc_building import build_documents_from_dataframe
+from config import COLLECTION_NAME, DB_DIR_LOCAL, EMB_DEVICE, EMB_MODEL_NAME, MARKDOWN_SEPARATORS, S3_BUCKET
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
@@ -14,55 +13,6 @@ from .utils_db import extract_paragraphs
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 # TODO : pourquoi on a les config dans ce fichier ?
-
-
-def build_database_from_dataframe(
-    df: pd.DataFrame,
-    persist_directory: str = str(DB_DIR_S3),
-) -> Chroma:
-    """
-    Args:
-        df (pd.DataFrame)
-
-    Returns:
-        Chroma: vector database
-    """
-    logging.info(f"The database will be stored in {persist_directory}")
-    # rename the column names:
-    not_null_filtered_df = df.rename(
-        columns={
-            "paragraphs": "content",
-            "url_source": "source",
-            "titles_para": "title",
-            "dateDiffusion": "date_diffusion",
-            "id_origin": "insee_id",
-        },
-        errors="raise",
-        inplace=False,
-    )
-
-    # chucking of documents
-    all_splits = build_documents_from_dataframe(not_null_filtered_df)
-    logging.info("Storing the Document objects")
-
-    embedding_model = HuggingFaceEmbeddings(  # load from sentence transformers
-        model_name=EMB_MODEL_NAME,
-        model_kwargs={"device": EMB_DEVICE},
-        encode_kwargs={"normalize_embeddings": True},  # set True for cosine similarity
-        show_progress=False,
-    )
-
-    # collection_name = "insee_data_" + str(EMB_MODEL_NAME.split("/")[-1])
-    logging.info("Building the vector database from the documents")
-    collection_name = COLLECTION_NAME
-    db = Chroma.from_documents(
-        collection_name=collection_name,
-        documents=all_splits,
-        persist_directory=persist_directory,
-        embedding=embedding_model,
-    )
-    logging.info("The database has been built")
-    return db
 
 
 def build_vector_database(
