391a8cfab956c115ef1fdf55745b23b44a3e58a4
mbesnier <matthias.besnier@dauphine.eu>
2024-07-12 18:09:33 +0000

    add new experiment notebook

--- src/db_building/__init__.py
+++ src/db_building/__init__.py
@@ -4,8 +4,11 @@ from .build_database import (
     reload_database_from_local_dir,
 )
 
+from .utils_db import extract_paragraphs
+
 __all__ = [
     "build_database_from_csv",
     "build_database_from_dataframe",
     "reload_database_from_local_dir",
+    "extract_paragraphs"
 ]
