8a4ce85133a12b3eb1b81afbbd425c12beb78003
linogaliana <lino.galiana@insee.fr>
2024-08-22 16:24:14 +0000

    pb now with argparse

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import argparse
 
 import chainlit as cl
 import chainlit.data as cl_data
@@ -27,12 +28,95 @@ logging.basicConfig(
 
 model = os.getenv("LLM_MODEL_NAME")
 quantization = True
-max_new_tokens = 10
-model_temperature = 1
+DEFAULT_MAX_NEW_TOKENS = 10
+DEFAULT_MODEL_TEMPERATURE = 1
 embedding = os.getenv("EMB_MODEL_NAME", EMB_MODEL_NAME)
 
 
 # APPLICATION -----------------------------------------
+
+parser = argparse.ArgumentParser(
+    description="Parameters to retrieve the correct LLM and DB from pretrained data"
+)
+
+## LLM specific arguments ----
+parser.add_argument(
+    "--llm_model",
+    type=str,
+    default=os.getenv("LLM_MODEL_NAME", "mistralai/Mistral-7B-Instruct-v0.2"),
+    help="""
+    LLM used to generate chat.
+    Should be a huggingface model.
+    Defaults to mistralai/Mistral-7B-Instruct-v0.2
+    """,
+)
+parser.add_argument(
+    "--quantization",
+    default=True,
+    action=argparse.BooleanOptionalAction,
+    help="""
+    Should we use a quantized version of "model" argument ?
+    --quantization yields True and --no-quantization yields False
+    """,
+)
+parser.add_argument(
+    "--max_new_tokens",
+    type=int,
+    default=DEFAULT_MAX_NEW_TOKENS,
+    help="""
+    The maximum numbers of tokens to generate, ignoring the number of tokens in the prompt.
+    See https://huggingface.co/docs/transformers/main_classes/text_generation
+    """,
+)
+parser.add_argument(
+    "--model_temperature",
+    type=int,
+    default=DEFAULT_MODEL_TEMPERATURE,
+    help="""
+    The value used to modulate the next token probabilities.
+    See https://huggingface.co/docs/transformers/main_classes/text_generation
+    """,
+)
+parser.add_argument(
+    "--return_full_text",
+    action=argparse.BooleanOptionalAction,
+    default=True,
+    help="""
+    Should we return the full text ?
+    --return_full_text yields True and --no-return_full_text yields False
+    Default to False
+    """,
+)
+parser.add_argument(
+    "--do_sample",
+    action=argparse.BooleanOptionalAction,
+    default=False,
+    help="""
+    if set to True , this parameter enables decoding strategies such as multinomial sampling, beam-search multinomial sampling, Top-K sampling
+    and Top-p sampling. All these strategies select the next token from the probability distribution over the entire vocabulary
+    with various strategy-specific adjustments.
+    --do_sample yields True and --no-do_sample yields False
+    Default to False
+    """,
+)
+
+
+args = parser.parse_args()
+
+def retrieve_model_and_tokenizer(
+    experiment_name: str,
+    **kwargs
+):
+  
+    llm, tokenizer = build_llm_model(
+            model_name=kwargs.get("llm_model"),
+            quantization_config=kwargs.get("quantization"),
+            config=True,
+            token=os.getenv("HF_TOKEN"),
+            streaming=False,
+            generation_args=kwargs,
+)
+    return llm, tokenizer
 
 
 @cl.on_chat_start
@@ -66,18 +150,8 @@ async def on_chat_start():
     cl.user_session.set("IS_LOGGING_ON", IS_LOGGING_ON)
 
     # Set Validator chain in chainlit session
-    llm, tokenizer = build_llm_model(
-        model_name=model,
-        quantization_config=quantization,
-        config=True,
-        token=os.getenv("HF_TOKEN"),
-        streaming=False,
-        generation_args={
-            "max_new_tokens": max_new_tokens,
-            "return_full_text": False,
-            "do_sample": False,
-            "temperature": model_temperature,
-        },
+    llm, tokenizer = retrieve_model_and_tokenizer(
+        **vars(args)
     )
     # Set Validator chain in chainlit session
     validator = build_chain_validator(evaluator_llm=llm, tokenizer=tokenizer)
@@ -101,18 +175,8 @@ async def on_chat_start():
     else:
         logging.info("------ chatbot mode : RAG")
 
-        llm, tokenizer = build_llm_model(
-            model_name=model,
-            quantization_config=quantization,
-            config=True,
-            token=os.getenv("HF_TOKEN"),
-            streaming=False,
-            generation_args={
-                "max_new_tokens": max_new_tokens,
-                "return_full_text": False,
-                "do_sample": True,
-                "temperature": model_temperature,
-            },
+        llm, tokenizer = retrieve_model_and_tokenizer(
+            **vars(args)
         )
         logging.info("------llm loaded")
 
