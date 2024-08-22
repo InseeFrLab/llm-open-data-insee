6bb9fa26e6866a6d0bde9bf5e14c475a9ec0f93a
linogaliana <lino.galiana@insee.fr>
2024-08-22 14:05:58 +0000

    correct

--- argo-workflows-templates/kill-failed-pods.sh
+++ argo-workflows-templates/kill-failed-pods.sh
@@ -6,4 +6,7 @@
 PATTERNS="run-evaluation-|run-build-database-|run-pipeline-"
 
 # Get the list of pod names that match the patterns and delete them
-kubectl delete pods $(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep -E "$PATTER
+kubectl delete pods $(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep -E "$PATTERNS")
+
+# Optional: Add a message to confirm the operation is complete
+echo "Pods matching the patterns have been deleted."
