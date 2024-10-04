189114281c8cea6dc18960a446de2ea6362f4db3
lgaliana <lino.galiana@insee.fr>
2024-10-04 10:03:16 +0000

    clean generation step

--- src/chain_building/build_chain.py
+++ src/chain_building/build_chain.py
@@ -21,7 +21,11 @@ RERANKER_CROSS_ENCODER = "BAAI/bge-reranker-base"
 def format_docs(docs: list):
     return "\n\n".join(
         [
-            f"Doc {i + 1}:\nTitle: {doc.metadata["titre"]}\nContent:\n{doc.page_content}"
+            f"""
+            Doc {i + 1}:\nTitle: {doc.metadata.get("Header 1")}\n
+            Source: {doc.metadata.get("url")}\n
+            Content:\n{doc.page_content}
+            """
             for i, doc in enumerate(docs)
         ]
     )
