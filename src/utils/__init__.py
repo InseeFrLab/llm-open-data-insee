079a2cf304bc3a5df3377028f3db9d0a8b14db5b
lgaliana <lino.galiana@insee.fr>
2024-12-03 16:21:55 +0000

    modularise again

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,12 @@
 from .loading_utilities import load_dataframe_from_parquet
 from .misc import compare_params
+from .formatting_utilities import (
+    create_prompt_from_instructions, format_docs
+)
 
-__all__ = ["load_dataframe_from_parquet", "cache_model_from_hf_hub", "compare_params"]
+__all__ = [
+    "load_dataframe_from_parquet", "cache_model_from_hf_hub",
+    "compare_params", "create_prompt_from_instructions",
+    "format_docs"
+]
+
