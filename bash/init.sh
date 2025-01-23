e3f2553994472e774b8b20165eb2f46f56403297
Lino Galiana <lino.galiana@insee.fr>
2025-01-23 13:07:03 +0100

    Update init.sh

--- bash/init.sh
+++ bash/init.sh
@@ -6,6 +6,11 @@ sudo apt install tree -y
 # Replace default flake8 linter with project-preconfigured ruff
 code-server --uninstall-extension ms-python.flake8
 code-server --install-extension charliermarsh.ruff
+code-server --install-extension oderwat.indent-rainbow
+code-server --install-extension pomdtr.excalidraw-editor
+code-server --install-extension tamasfe.even-better-toml
+code-server --install-extension aaron-bond.better-comments
+code-server --install-extension github.vscode-github-actions
 
 # Install type checking extension
 code-server --install-extension ms-python.mypy-type-checker
@@ -60,6 +65,25 @@ pre-commit run --all-files
 echo "Running nbstripout --install..."
 nbstripout --install
 
+
+
+
+# VSCODE PERSONAL SETTINGS -----------------------
+
+jq '. + {
+    "workbench.colorTheme": "Default Dark Modern",  # Set the theme
+
+    "editor.rulers": [80, 100, 120],  # Add specific vertical rulers
+    "files.trimTrailingWhitespace": true,  # Automatically trim trailing whitespace
+    "files.insertFinalNewline": true,  # Ensure files end with a newline
+
+    "flake8.args": [
+        "--max-line-length=100"  # Max line length for Python linting
+    ]
+
+
+    
+}' "$SETTINGS_FILE" > "$SETTINGS_FILE.tmp" && mv "$SETTINGS_FILE.tmp" "$SETTINGS_FILE"
 
 
 # mc cp -r s3/projet-llm-insee-open-data/data/chroma_database/chroma_db/ ~/work/data/chroma_db
