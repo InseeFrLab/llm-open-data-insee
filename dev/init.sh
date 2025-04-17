8e4ce6025efe802b04aa3a0613f6b8206a6fb812
Lino Galiana <lino.galiana@insee.fr>
2025-04-17 19:03:17 +0200

    Update init.sh

--- dev/init.sh
+++ dev/init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Run linoprefs setup if the argument "linoprefs" is passed
+if [[ "$1" == "linoprefs" ]]; then
+  curl -sS https://raw.githubusercontent.com/linogaliana/init-scripts/refs/heads/main/install-copilot.sh | bash
+fi
+
+# Clone the repository and set up the environment
 git clone https://github.com/InseeFrLab/llm-open-data-insee.git
 cd llm-open-data-insee
 
